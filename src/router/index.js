c00a637d53da2b752eb0f746f6f77892d422ab1d
Daniel Neyla <daniel.neyla@gmail.com>
2022-11-13 21:59:29 +0000

    made a link to register for an event and to added an event with nested Routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,11 @@ import { createRouter, createWebHistory } from "vue-router";
 import EventList from "../views/EventList.vue";
 import AboutView from "../views/AboutView.vue";
 
-import EventDetails from "../views/EventDetails.vue";
+import EventDetails from "../views/event/details.vue";
+import EventRegister from "../views/event/register.vue";
+import EventEdit from "../views/event/edit.vue";
+import EventLayout from "../views/event/layout.vue";
+
 
 const routes = [
   {
@@ -21,12 +25,36 @@ const routes = [
     // component: () =>
     //   import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
+
+
   {
     path: "/event/:id",
-    name: "EventDetails",
+    name: "EventLayout",
     props: true,
-    component: EventDetails,
+    component: EventLayout,
+    children: [
+      {
+        path: '',
+        name: "EventDetails",
+        component: EventDetails,
+      },
+
+      {
+        path: "register",
+        name: "EventRegister",
+
+        component: EventRegister,
+      },
+      {
+        path: "edit",
+        name: "EventEdit",
+
+        component: EventEdit,
+      },
+    ]
   },
+
+
 ];
 
 const router = createRouter({
---
+++ src/views/event/edit.vue
@@ -0,0 +1,14 @@
+<template>
+  <div v-if="event">
+    <p>Edit for Event here</p>
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  props: ['event']
+};
+</script>
